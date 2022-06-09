d239b75dd00300e29af913214c07f584f51f4ecb
Arthur <arthur_2335@hotmail.com>
2022-06-09 10:49:45 -0400

    fixed .data issue to get token

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,7 @@ import { profile } from "./src/getProfile.js";
 import { dbConnect } from "./src/connectDB.js";
 import jwt from "jsonwebtoken";
 import mySecretKey from "./secret.js";
+import admin from "firebase-admin";
 
 const app = express();
 app.use(cors());
@@ -16,14 +17,17 @@ app.get("/test", (req, res) => {
   res.send("Yaaay we are connected!!!");
 });
 
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
   const { email, password } = req.body;
-  let user = users.find(
-    (user) => user.email === email && user.password === password
-  );
+  const userCol = await db
+    .collection("UserProfiles")
+    .where("email", "==", email.toLowerCase())
+    .where("password", "==", password)
+    .get();
+  let user = userCol.docs[0].data();
   if (!user) {
     res
-      .status(400)
+      .status(401)
       .send({ error: "Invalid Email or Password, please try again." });
     return;
   }
@@ -40,11 +44,9 @@ app.get("/private", (req, res) => {
   }
   jwt.verify(token, mySecretKey, (err, decoded) => {
     if (err) {
-      res
-        .status(401)
-        .send({
-          error: "You must use a valid authentication token to see this" + err,
-        });
+      res.status(401).send({
+        error: "You must use a valid authentication token to see this" + err,
+      });
       return;
     }
     res.send({ message: `Welcome ${decoded.rideOwner}!` });
